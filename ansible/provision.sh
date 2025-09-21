93524cf087d26ee4a07cf19d3095e702c4d37597
Gediminas Repecka <gediminas.repecka@gmail.com>
2025-09-21 23:21:28 +0300

    Update ansible with rpi PoE hat temp config setup

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 ansible-galaxy collection install git+https://github.com/k3s-io/k3s-ansible.git
-ansible-playbook k3s.orchestration.site -i inventory.yml --vault-password-file vault-pass.txt --become-password-file become-pass.txt
+ansible-playbook k3s.orchestration.site -i inventory.yaml --vault-password-file vault-pass.txt --become-password-file become-pass.txt
+ansible-playbook playbooks/rpi-fan.yaml -i inventory.yaml
