b0b4d6926b0178b53f38fb727160e11b58afa410
Gediminas Repecka <gediminas.repecka@gmail.com>
2025-09-25 17:41:01 +0300

    add longhorn, update ansible

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -4,4 +4,5 @@ ansible-galaxy collection install git+https://github.com/k3s-io/k3s-ansible.git
 ansible-playbook k3s.orchestration.site -i inventory.yaml --vault-password-file vault-pass.txt --become-password-file become-pass.txt
 ansible-playbook playbooks/rpi-setup.yaml -i inventory.yaml
 kubectl kustomize ../apps/cilium --enable-helm | kubectl apply -f -
+ansible-playbook playbooks/secret.yaml --vault-password-file vault-pass.txt # install secret for sealed secrets operator
 kubectl apply -k ../argocd || kubectl apply -k ../argocd
