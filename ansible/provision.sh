adf4bcb73cfd6b43b64ab44021fb56df6f9cf76a
Gediminas Repecka <gediminas.repecka@gmail.com>
2025-09-29 01:07:28 +0300

    install gateway api crds via ansible, enable gateway api in cilium

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -4,5 +4,5 @@ ansible-galaxy collection install git+https://github.com/k3s-io/k3s-ansible.git
 ansible-playbook k3s.orchestration.site -i inventory.yaml --vault-password-file vault-pass.txt --become-password-file become-pass.txt
 ansible-playbook playbooks/rpi-setup.yaml -i inventory.yaml
 kubectl kustomize ../apps/cilium --enable-helm | kubectl apply -f -
-ansible-playbook playbooks/secret.yaml --vault-password-file vault-pass.txt # install secret for sealed secrets operator
+ansible-playbook playbooks/bootstrap.yaml --vault-password-file vault-pass.txt
 kubectl apply -k ../argocd || kubectl apply -k ../argocd
