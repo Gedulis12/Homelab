de0306e69af4e2bd3da3853b5c71dca207da5f74
Gediminas Repecka <gediminas.repecka@gmail.com>
2025-09-29 20:14:29 +0300

    update provisioning script

--- ansible/provision.sh
+++ ansible/provision.sh
@@ -3,6 +3,6 @@
 ansible-galaxy collection install git+https://github.com/k3s-io/k3s-ansible.git
 ansible-playbook k3s.orchestration.site -i inventory.yaml --vault-password-file vault-pass.txt --become-password-file become-pass.txt
 ansible-playbook playbooks/rpi-setup.yaml -i inventory.yaml
-kubectl kustomize ../apps/cilium --enable-helm | kubectl apply -f -
 ansible-playbook playbooks/bootstrap.yaml --vault-password-file vault-pass.txt
+kubectl kustomize ../apps/cilium --enable-helm | kubectl apply -f -
 kubectl apply -k ../argocd || kubectl apply -k ../argocd
